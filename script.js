cf5c6735d2ded9d3561c1e9daf38af2c78d574b6
sampteken-cmd <sampteken@gmail.com>
2025-08-29 21:05:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Animações suaves com ScrollReveal
+ScrollReveal().reveal('header', { 
+    duration: 1000, 
+    origin: 'top', 
+    distance: '50px' 
+});
+
+ScrollReveal().reveal('#sobre .container', { 
+    duration: 1000, 
+    origin: 'left', 
+    distance: '50px', 
+    delay: 200
+});
+
+ScrollReveal().reveal('#exemplos .container', { 
+    duration: 1000, 
+    origin: 'bottom', 
+    distance: '50px', 
+    delay: 200
+});
+
+ScrollReveal().reveal('#contato .container', { 
+    duration: 1000, 
+    origin: 'right', 
+    distance: '50px', 
+    delay: 200
+});
+
+ScrollReveal().reveal('#comprar .container', { 
+    duration: 1000, 
+    origin: 'bottom', 
+    distance: '50px', 
+    delay: 200
+});
